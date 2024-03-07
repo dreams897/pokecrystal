6bd2738b933c53991da8441435995a28c29589f7
dreams897 <dreams897@proton.me>
2024-03-06 17:23:06 -0800

    Fixed Museum warps

--- maps/PewterMuseum1F.asm
+++ maps/PewterMuseum1F.asm
@@ -9,9 +9,9 @@ PewterMuseum1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  11, 7, PEWTER_CITY, 2
-	warp_event  10, 7, PEWTER_CITY, 2
 	warp_event  7, 7,  PEWTER_MUSEUM_2F, 1
+	warp_event  10, 7, PEWTER_CITY, 6
+	warp_event  11, 7, PEWTER_CITY, 6
 
 	def_coord_events
 
