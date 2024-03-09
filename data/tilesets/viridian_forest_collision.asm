2a22d6d41c91623d232649e4d8448fb9c8c59a63
dreams897 <119394167+dreams897@users.noreply.github.com>
2024-03-09 13:43:12 -0800

    Fix Viridian Forest collision

--- data/tilesets/viridian_forest_collision.asm
+++ data/tilesets/viridian_forest_collision.asm
@@ -21,108 +21,53 @@
 	tilecoll FLOOR, FLOOR, HEADBUTT_TREE, HEADBUTT_TREE ; 14
 	tilecoll FLOOR, WALL, WALL, WALL ; 15
 	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 16
-	tilecoll FLOOR, FLOOR, FLOOR, HEADBUTT_TREE ; 17
-	tilecoll WALL, HOP_LEFT, WALL, HOP_LEFT ; 18
-	tilecoll WALL, HOP_DOWN_LEFT, WALL, WALL ; 19
-	tilecoll HOP_DOWN, HOP_DOWN, WALL, WALL ; 1a
-	tilecoll HOP_DOWN_RIGHT, WALL, WALL, WALL ; 1b
-	tilecoll HOP_DOWN, FLOOR, WALL, FLOOR ; 1c
-	tilecoll WALL, WALL, WALL, WALL ; 1d
-	tilecoll WALL, WALL, WALL, WALL ; 1e
-	tilecoll WALL, WALL, WALL, WALL ; 1f
-	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 20
-	tilecoll WALL, WALL, WALL, WALL ; 21
-	tilecoll WALL, WALL, WALL, WALL ; 22
-	tilecoll WALL, WALL, WALL, WALL ; 23
-	tilecoll WALL, WALL, WALL, DOOR ; 24
-	tilecoll WATER, WATER, WATER, WATER ; 25
-	tilecoll WATER, WATER, WATER, WATER ; 26
-	tilecoll WALL, WALL, WALL, WALL ; 27
-	tilecoll , , ,  ; 28
-	tilecoll , , ,  ; 29
-	tilecoll , , ,  ; 2a
-	tilecoll , , ,  ; 2b
-	tilecoll , , ,  ; 2c
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 17
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 18
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 19
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1a
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1b
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1c
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1d
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1e
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1f
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 20
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 21
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 22
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 23
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 24
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 25
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 26
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 27
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 28
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 29
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2a
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2b
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2c
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2d
-	tilecoll , , ,  ; 2e
-	tilecoll , , ,  ; 2f
-	tilecoll , , ,  ; 30
-	tilecoll , , ,  ; 31
-	tilecoll , , ,  ; 32
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2e
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2f
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 30
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 31
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 32
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 33
-	tilecoll , , ,  ; 34
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 34
 	tilecoll WALL, WALL, FLOOR, WALL ; 35
-	tilecoll , , ,  ; 36
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 36
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 37
-	tilecoll , , ,  ; 38
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 38
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 39
-	tilecoll , , ,  ; 3a
-	tilecoll , , ,  ; 3b
-	tilecoll , , ,  ; 3c
-	tilecoll , , ,  ; 3d
-	tilecoll , , ,  ; 3e
-	tilecoll , , ,  ; 3f
-	tilecoll , , ,  ; 40
-	tilecoll , , ,  ; 41
-	tilecoll , , ,  ; 42
-	tilecoll , , ,  ; 43
-	tilecoll , , ,  ; 44
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3a
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3b
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3c
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3d
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3e
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 3f
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 40
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 41
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 42
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 43
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 44
 	tilecoll FLOOR, FLOOR, FLOOR, WARP_CARPET_DOWN ; 45
 	tilecoll WARP_CARPET_DOWN, WARP_CARPET_DOWN, WARP_CARPET_DOWN, WARP_CARPET_DOWN ; 46
 	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, FLOOR ; 47
 	tilecoll WARP_CARPET_UP, WARP_CARPET_UP, FLOOR, FLOOR ; 48
-	tilecoll , , ,  ; 49
-	tilecoll , , ,  ; 4a
-	tilecoll , , ,  ; 4b
-	tilecoll , , ,  ; 4c
-	tilecoll , , ,  ; 4d
-	tilecoll , , ,  ; 4e
-	tilecoll , , ,  ; 4f
-	tilecoll , , ,  ; 50
-	tilecoll , , ,  ; 51
-	tilecoll , , ,  ; 52
-	tilecoll , , ,  ; 53
-	tilecoll , , ,  ; 54
-	tilecoll , , ,  ; 55
-	tilecoll , , ,  ; 56
-	tilecoll , , ,  ; 57
-	tilecoll , , ,  ; 58
-	tilecoll , , ,  ; 59
-	tilecoll , , ,  ; 5a
-	tilecoll , , ,  ; 5b
-	tilecoll , , ,  ; 5c
-	tilecoll , , ,  ; 5d
-	tilecoll , , ,  ; 5e
-	tilecoll , , ,  ; 5f
-	tilecoll , , ,  ; 60
-	tilecoll , , ,  ; 61
-	tilecoll , , ,  ; 62
-	tilecoll , , ,  ; 63
-	tilecoll , , ,  ; 64
-	tilecoll , , ,  ; 65
-	tilecoll , , ,  ; 66
-	tilecoll , , ,  ; 67
-	tilecoll , , ,  ; 68
-	tilecoll , , ,  ; 69
-	tilecoll , , ,  ; 6a
-	tilecoll , , ,  ; 6b
-	tilecoll , , ,  ; 6c
-	tilecoll , , ,  ; 6d
-	tilecoll , , ,  ; 6e
-	tilecoll , , ,  ; 6f
-	tilecoll , , ,  ; 70
-	tilecoll , , ,  ; 71
-	tilecoll , , ,  ; 72
-	tilecoll , , ,  ; 73
-	tilecoll , , ,  ; 74
-	tilecoll , , ,  ; 75
-	tilecoll , , ,  ; 76
-	tilecoll , , ,  ; 77
-	tilecoll , , ,  ; 78
-	tilecoll , , ,  ; 79
-	tilecoll , , ,  ; 7a
-	tilecoll , , ,  ; 7b
-	tilecoll , , ,  ; 7c
-	tilecoll , , ,  ; 7d
-	tilecoll , , ,  ; 7e
-	tilecoll , , ,  ; 7f
