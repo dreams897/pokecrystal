e7243fa08d37bb4eb588877733f1920d35ecc544
dreams897 <dreams897@proton.me>
2024-03-11 17:43:57 -0700

    Youngster on Route 1 gives out Potion

--- maps/Route1.asm
+++ maps/Route1.asm
@@ -1,6 +1,6 @@
 	object_const_def
-	const ROUTE1_YOUNGSTER1
-	const ROUTE1_YOUNGSTER2
+	const ROUTE1_YOUNGSTER
+	const ROUTE1_YOUNGSTER_GIVE_POTION
 	const ROUTE1_FRUIT_TREE
 
 Route1_MapScripts:
@@ -10,9 +10,55 @@ Route1_MapScripts:
 	
 Route1Youngster1Script:
 	end
+	
+Route1YoungsterGivePotionScript:
+	opentext
+	checkevent EVENT_GOT_POTION_SAMPLE
+	iftrue .AlsoGotPokeballsText
+	setevent EVENT_GOT_POTION_SAMPLE
+	writetext Route1Youngster1MartSampleText
+	promptbutton
+	getitemname STRING_BUFFER_4, POTION
+	scall Youngster_Give_Potion
+	giveitem POTION
+	itemnotify
+	closetext
+	end
+	
+.AlsoGotPokeballsText
+	writetext Route1Youngster1AlsoGotPokeballsText
+
+Youngster_Give_Potion:
+	jumpstd ReceiveItemScript
+	end
 
 Route1Youngster2Script:
 	end
+	
+Route1Youngster1MartSampleText:
+	text "Hi! I work at a"
+	line "#MON MART."
+
+	para "It's a convenient"
+	line "shop, so please"
+	cont "visit us in"
+	cont "VIRIDIAN CITY."
+
+	para "I know, I'll give"
+	line "you a sample!"
+	cont "Here you go!"
+	prompt
+
+Route1Youngster1AlsoGotPokeballsText:
+	text "We also carry"
+	line "# BALLs for"
+	cont "catching #MON!"
+	done
+
+Route1Youngster1NoRoomText:
+	text "You have too much"
+	line "stuff with you!"
+	done
 
 Route1Sign:
 	jumptext Route1SignText
@@ -39,5 +85,5 @@ Route1_MapEvents:
 
 	def_object_events
 	object_event  4, 12, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, Route1Youngster1Script, -1
-	object_event  9, 25, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route1Youngster2Script, -1
+	object_event  9, 25, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, Route1YoungsterGivePotionScript, -1
 	object_event  3,  7, SPRITE_FRUIT_TREE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route1FruitTree, -1
