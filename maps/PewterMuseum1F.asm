1d2c20bf68ebabeb0aaf7dcd9b606f90d0878106
dreams897 <dreams897@proton.me>
2024-03-13 17:58:00 -0700

    Pewter Museum improvements

--- maps/PewterMuseum1F.asm
+++ maps/PewterMuseum1F.asm
@@ -28,9 +28,11 @@ PewterMuseum1F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  7, 7,  PEWTER_MUSEUM_2F, 1
 	warp_event  10, 7, PEWTER_CITY, 6
 	warp_event  11, 7, PEWTER_CITY, 6
+	warp_event 16,  7, PEWTER_CITY, 7
+	warp_event 17,  7, PEWTER_CITY, 7
+	warp_event  7, 7,  PEWTER_MUSEUM_2F, 1
 
 	def_coord_events
 
--- maps/PewterMuseum2F.asm
+++ maps/PewterMuseum2F.asm
@@ -2,15 +2,21 @@ PewterMuseum2F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	
+PewterMuseum2FSpaceShuttleSignpostText:
+	text "Space Shuttle"
+	done
 
 PewterMuseum2F_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event  7, 7,  PEWTER_MUSEUM_1F, 1
+	warp_event  7, 7,  PEWTER_MUSEUM_1F, 5
 
 	def_coord_events
 
 	def_bg_events
+	bg_event 11,  2, BGEVENT_READ, PewterMuseum2FSpaceShuttleSignpostText
 
 	def_object_events
+
