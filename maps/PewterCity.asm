fc32452d0042c0b76fd7c91ee3dc3a4e55a18509
dreams897 <dreams897@proton.me>
2024-03-20 13:12:27 -0700

    Fixed Pewter City warps

--- maps/PewterCity.asm
+++ maps/PewterCity.asm
@@ -160,7 +160,7 @@ PewterCity_MapEvents:
 	warp_event 29, 13, PEWTER_NIDORAN_SPEECH_HOUSE, 1
 	warp_event 23, 17, PEWTER_MART, 1
 	warp_event  7, 29, PEWTER_SNOOZE_SPEECH_HOUSE, 1
-	warp_event 13, 25, PEWTER_POKECENTER, 1
+	warp_event 13, 25, PEWTER_POKECENTER_1F, 1
 
 	def_coord_events
 
