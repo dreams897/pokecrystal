e852052377b890fa44f5151048f1868ae62e69dd
dreams897 <dreams897@proton.me>
2024-03-22 15:30:10 -0700

    Add Viridian Forest N/S gate scripts

--- maps/Viridian_Forest_North_Gate.asm
+++ maps/Viridian_Forest_North_Gate.asm
@@ -1,9 +1,37 @@
 	object_const_def
+	const VIRIDIAN_FOREST_NORTH_GATE_SUPER_NERD
+	const VIRIDIAN_FOREST_NORTH_GATE_GRAMPS
 
 ViridianForestNorthGate_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	
+ViridianForestNorthGateSuperNerdScript:
+	jumptextfaceplayer ViridianForestNorthGateSuperNerdText
+	
+ViridianForestNorthGateGrampsScript:
+	jumptextfaceplayer ViridianForestNorthGateGrampsText
+	
+ViridianForestNorthGateSuperNerdText:
+	text "Many #MON live"
+	line "only in forests "
+	cont "and caves."
+
+	para "You need to look"
+	line "everywhere to get"
+	cont "different kinds!"
+	done
+
+ViridianForestNorthGateGrampsText:
+	text "Have you noticed"
+	line "the bushes on the"
+	cont "roadside?"
+
+	para "They can be cut"
+	line "down by a special"
+	cont "#MON move."
+	done
 
 ViridianForestNorthGate_MapEvents:
 	db 0, 0 ; filler
@@ -19,3 +47,5 @@ ViridianForestNorthGate_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event  3,  2, SPRITE_SUPER_NERD, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, ViridianForestNorthGateSuperNerdScript, -1
+	object_event  2,  5, SPRITE_GRAMPS, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, ViridianForestNorthGateGrampsScript, -1
--- maps/Viridian_Forest_South_Gate.asm
+++ maps/Viridian_Forest_South_Gate.asm
@@ -1,9 +1,31 @@
 	object_const_def
+	const VIRIDIAN_FOREST_SOUTH_GATE_GIRL
+	const VIRIDIAN_FOREST_SOUTH_GATE_LITTLE_GIRL
 
 ViridianForestSouthGate_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	
+ViridianForestSouthGateGirlScript:
+	jumptextfaceplayer ViridianForestSouthGateGirlText
+	
+ViridianForestSouthGateLittleGirlScript:
+	jumptextfaceplayer ViridianForestSouthGateLittleGirlText
+	
+ViridianForestSouthGateGirlText:
+	text "Are you going to"
+	line "VIRIDIAN FOREST?"
+	cont "Be careful, it's"
+	cont "a natural maze!"
+	done
+
+ViridianForestSouthGateLittleGirlText:
+	text "RATTATA may be"
+	line "small, but its"
+	cont "bite is wicked!"
+	cont "Did you get one?"
+	done
 
 ViridianForestSouthGate_MapEvents:
 	db 0, 0 ; filler
@@ -19,3 +41,5 @@ ViridianForestSouthGate_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event  8,  4, SPRITE_GIRL, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, ViridianForestSouthGateGirlScript, -1
+	object_event  2,  4, SPRITE_LITTLE_GIRL, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, ViridianForestSouthGateLittleGirlScript, -1
