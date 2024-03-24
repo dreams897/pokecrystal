4307e991c41dd79de3b58b10e183accde89ba0b1
dreams897 <dreams897@proton.me>
2024-03-23 20:58:20 -0700

    Improve old man catching tutorial

--- engine/gfx/color.asm
+++ engine/gfx/color.asm
@@ -686,6 +686,9 @@ GetPlayerOrMonPalettePointer:
 	ld a, [wPlayerSpriteSetupFlags]
 	bit PLAYERSPRITESETUP_FEMALE_TO_MALE_F, a
 	jr nz, .male
+	ld a, [wBattleType]
+	cp BATTLETYPE_TUTORIAL
+	jr z, .dude
 	ld a, [wPlayerGender]
 	and a
 	jr z, .male
@@ -695,6 +698,12 @@ GetPlayerOrMonPalettePointer:
 .male
 	ld hl, PlayerPalette
 	ret
+
+.dude
+	ld hl, DudePalette
+	ret
+	
+INCLUDE "gfx/player/dude.pal"
 
 GetFrontpicPalettePointer:
 	and a
