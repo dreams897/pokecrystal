6cc622e2c30651128faf5c33543ea86215531cd5
dreams897 <119394167+dreams897@users.noreply.github.com>
2024-03-26 18:10:28 -0700

    Improve Kanto House collision

--- data/tilesets/house_kanto_collision.asm
+++ data/tilesets/house_kanto_collision.asm
@@ -1,6 +1,6 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 00
-	tilecoll FLOOR, WALL, FLOOR, WALL ; 01
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 02
+	tilecoll WALL, WALL, WALL, WALL ; 01
+	tilecoll WALL, WALL, WALL, WALL ; 02
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 03
 	tilecoll WALL, WALL, BOOKSHELF, BOOKSHELF ; 04
 	tilecoll WALL, WINDOW, FLOOR, FLOOR ; 05
@@ -18,33 +18,33 @@
 	tilecoll WALL, WALL, WALL, FLOOR ; 11
 	tilecoll WALL, WALL, FLOOR, STAIRCASE ; 12
 	tilecoll WALL, TOWN_MAP, FLOOR, FLOOR ; 13
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 14
-	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 15
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 14
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 15
 	tilecoll WALL, WINDOW, FLOOR, FLOOR ; 16
 	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 17
-	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 18
-	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 19
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 18
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 19
 	tilecoll WALL, WALL, BOOKSHELF, BOOKSHELF ; 1a
 	tilecoll WALL, DOOR, FLOOR, FLOOR ; 1b
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 1c
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 1d
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 1e
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 1f
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 20
+	tilecoll FLOOR, FLOOR, WALL, WALL ; 20
 	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 21
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 22
 	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 23
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 24
 	tilecoll FLOOR, FLOOR, WALL, WALL ; 25
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 26
-	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 27
+	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 26
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 27
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 28
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 29
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 2a
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 2b
 	tilecoll WALL, WALL, WALL, WALL ; 2c
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 2d
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 2e
+	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 2e
 	tilecoll WALL, WALL, FLOOR, WALL ; 2f
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 30
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 31
