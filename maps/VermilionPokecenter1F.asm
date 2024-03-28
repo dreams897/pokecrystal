ee6c4d84e0ef368cfb208f935976f033ff14d97f
dreams897 <dreams897@proton.me>
2024-03-28 00:15:15 -0700

    Add Vermilion Pokecenter script

--- maps/VermilionPokecenter1F.asm
+++ maps/VermilionPokecenter1F.asm
@@ -1,8 +1,9 @@
 	object_const_def
 	const VERMILIONPOKECENTER1F_NURSE
 	const VERMILIONPOKECENTER1F_FISHING_GURU
-	const VERMILIONPOKECENTER1F_SAILOR
-	const VERMILIONPOKECENTER1F_BUG_CATCHER
+	const VERMILIONPOKECENTER_SAILOR
+	const VERMILIONPOKECENTER_BENCH_GUY
+	const VERMILIONPOKECENTER1F_LINK_RECEPTIONIST
 
 VermilionPokecenter1F_MapScripts:
 	def_scene_scripts
@@ -11,67 +12,66 @@ VermilionPokecenter1F_MapScripts:
 
 VermilionPokecenter1FNurseScript:
 	jumpstd PokecenterNurseScript
-
-VermilionPokecenter1FFishingGuruScript:
+	
+VermilionPokecenterLinkScript:
+	jumptext VermilionPokecenterLinkText
+	
+VermilionPokecenterFishingGuruScript:
+	jumptextfaceplayer VermilionPokecenterFishingGuruText
+	
+VermilionPokecenterSailorScript:
+	jumptextfaceplayer VermilionPokecenterSailorText
+	
+VermilionPokecenterBenchGuyScript:
 	faceplayer
 	opentext
-	checkevent EVENT_FOUGHT_SNORLAX
-	iftrue .FoughtSnorlax
-	writetext VermilionPokecenter1FFishingGuruText
+	writetext VermilionPokecenterBenchGuyText
 	waitbutton
 	closetext
+	turnobject VERMILIONPOKECENTER_BENCH_GUY, RIGHT
 	end
+	
+VermilionPokecenterFishingGuruText:
+	text "Even if they are"
+	line "the same level,"
+	cont "#MON can have"
+	cont "very different"
+	cont "abilities."
 
-.FoughtSnorlax:
-	writetext VermilionPokecenter1FFishingGuruText_FoughtSnorlax
-	waitbutton
-	closetext
-	end
-
-VermilionPokecenter1FSailorScript:
-	jumptextfaceplayer VermilionPokecenter1FSailorText
-
-VermilionPokecenter1FBugCatcherScript:
-	jumptextfaceplayer VermilionPokecenter1FBugCatcherText
-
-VermilionPokecenter1FFishingGuruText:
-	text "A sleeping #MON"
-	line "is lying in front"
-	cont "of DIGLETT'S CAVE."
-
-	para "It's a fantastic"
-	line "opportunity to get"
-
-	para "it, but how do you"
-	line "wake it up?"
+	para "A #MON raised"
+	line "by a trainer is"
+	cont "stronger than one"
+	cont "in the wild."
 	done
 
-VermilionPokecenter1FFishingGuruText_FoughtSnorlax:
-	text "There used to be a"
-	line "sleeping #MON"
-
-	para "lying in front of"
-	line "DIGLETT'S CAVE."
-
-	para "But it seems to"
-	line "have disappeared."
+VermilionPokecenterSailorText:
+	text "My #MON was"
+	line "poisoned! It"
+	cont "fainted while we"
+	cont "were walking!"
 	done
 
-VermilionPokecenter1FSailorText:
-	text "The FAST SHIP is a"
-	line "great place to"
+VermilionPokecenterBenchGuyText:
+	text "It is true that a"
+	line "higher level"
+	cont "#MON will be"
+	cont "more powerful..."
 
-	para "meet and battle"
-	line "trainers."
+	para "But, all #MON"
+	line "will have weak"
+	cont "points against"
+	cont "specific types."
+
+	para "So, there is no"
+	line "universally"
+	cont "strong #MON."
 	done
-
-VermilionPokecenter1FBugCatcherText:
-	text "Oh? You have some"
-	line "BADGES I've never"
-	cont "seen before."
-
-	para "Oh, I get it. You"
-	line "got them in JOHTO."
+	
+VermilionPokecenterLinkText:
+	text "This area is"
+	line "reserved for 2"
+	cont "friends who are"
+	cont "linked by cable."
 	done
 
 VermilionPokecenter1F_MapEvents:
@@ -80,7 +80,6 @@ VermilionPokecenter1F_MapEvents:
 	def_warp_events
 	warp_event  3,  7, VERMILION_CITY, 2
 	warp_event  4,  7, VERMILION_CITY, 2
-	warp_event  0,  7, POKECENTER_2F, 1
 
 	def_coord_events
 
@@ -88,6 +87,7 @@ VermilionPokecenter1F_MapEvents:
 
 	def_object_events
 	object_event  3,  1, SPRITE_NURSE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FNurseScript, -1
-	object_event  7,  2, SPRITE_FISHING_GURU, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FFishingGuruScript, -1
-	object_event  6,  5, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_UP_DOWN, 0, 1, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FSailorScript, -1
-	object_event  1,  5, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, VermilionPokecenter1FBugCatcherScript, -1
+	object_event  10, 5, SPRITE_FISHING_GURU, SPRITEMOVEDATA_WANDER, 0, 0, -1, -1, 0, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, VermilionPokecenterFishingGuruScript, -1
+	object_event  5,  4, SPRITE_SAILOR, SPRITEMOVEDATA_WANDER, 0, 0, -1, -1, 0, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, VermilionPokecenterSailorScript, -1
+	object_event  0,  4, SPRITE_BENCH_GUY, SPRITEMOVEDATA_STANDING_RIGHT, 1, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, VermilionPokecenterBenchGuyScript, -1
+	object_event  11, 2, SPRITE_OLD_LINK_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, VermilionPokecenterLinkScript, -1
