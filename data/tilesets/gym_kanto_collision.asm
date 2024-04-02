d1aa8c77e388701522135b20cbe4926e22d05e0e
dreams897 <119394167+dreams897@users.noreply.github.com>
2024-04-02 14:13:34 -0700

    Add priority tiles and fix missing tile Gen 1 gym tileset

--- data/tilesets/gym_kanto_collision.asm
+++ data/tilesets/gym_kanto_collision.asm
@@ -4,8 +4,8 @@
 	tilecoll WALL, WALL, WALL, WALL ; 03
 	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, WARP_CARPET_DOWN ; 04
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 05
-	tilecoll FLOOR, FLOOR, WALL, WALL ; 06
-	tilecoll FLOOR, FLOOR, WALL, WALL ; 07
+	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 06
+	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 07
 	tilecoll WALL, WALL, WALL, FLOOR ; 08
 	tilecoll WALL, WALL, FLOOR, WALL ; 09
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 0a
@@ -23,8 +23,8 @@
 	tilecoll WATER, FLOOR, WATER, FLOOR ; 16
 	tilecoll WATER, WATER, WATER, WATER ; 17
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 18
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 19
-	tilecoll WATER, WALL, WATER, WALL ; 1a
+	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 19
+	tilecoll WATER, FLOOR, WATER, WALL ; 1a
 	tilecoll FLOOR, FLOOR, WATER, WATER ; 1b
 	tilecoll FLOOR, WATER, FLOOR, WATER ; 1c
 	tilecoll WATER, FLOOR, WATER, FLOOR ; 1d
@@ -38,8 +38,8 @@
 	tilecoll WALL, WALL, FLOOR, FLOOR ; 25
 	tilecoll WALL, BOOKSHELF, WALL, BOOKSHELF ; 26
 	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 27
-	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 28
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 29
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 28
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 29
 	tilecoll WALL, WALL, WALL, WALL ; 2a
 	tilecoll WALL, WALL, WALL, WALL ; 2b
 	tilecoll WALL, WALL, WALL, WALL ; 2c
@@ -47,8 +47,8 @@
 	tilecoll FLOOR, FLOOR, WALL, WALL ; 2e
 	tilecoll WALL, FLOOR, WALL, FLOOR ; 2f
 	tilecoll FLOOR, WALL, FLOOR, WALL ; 30
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 31
-	tilecoll FLOOR, WALL, FLOOR, WALL ; 32
+	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 31
+	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 32
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 33
 	tilecoll WALL, WALL, WALL, WALL ; 34
 	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 35
@@ -113,4 +113,4 @@
 	tilecoll LADDER, WALL, FLOOR, WALL ; 70
 	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 71
 	tilecoll WALL, WALL, WALL, FLOOR ; 72
-	tilecoll FLOOR, WALL, FLOOR, WALL ; 73
+	tilecoll WALL, WALL, FLOOR, WALL ; 73
