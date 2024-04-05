8ac23a748e46982347631086d8a807c8996bf0ae
dreams897 <dreams897@proton.me>
2024-04-05 11:35:49 -0700

    Start working on Mt. Moon scripts

--- maps/MtMoon1F.asm
+++ maps/MtMoon1F.asm
@@ -4,6 +4,126 @@ MtMoon1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	
+MtMoon1FHikerBattleText:
+	text "WHOA! You shocked"
+	line "me! Oh, you're"
+	cont "just a kid!"
+	done
+
+MtMoon1FHikerEndBattleText:
+	text "Wow!"
+	line "Shocked again!"
+	prompt
+
+MtMoon1FHikerAfterBattleText:
+	text "Kids like you"
+	line "shouldn't be"
+	cont "here!"
+	done
+
+MtMoon1FYoungster1BattleText:
+	text "Did you come to"
+	line "explore too?"
+	done
+
+MtMoon1FYoungster1EndBattleText:
+	text "Losing"
+	line "stinks!"
+	prompt
+
+MtMoon1FYoungster1AfterBattleText:
+	text "I came down here"
+	line "to show off to"
+	cont "girls."
+	done
+
+MtMoon1FCooltrainerF1BattleText:
+	text "Wow! It's way"
+	line "bigger in here"
+	cont "than I thought!"
+	done
+
+MtMoon1FCooltrainerF1EndBattleText:
+	text "Oh!"
+	line "I lost it!"
+	prompt
+
+MtMoon1FCooltrainerF1AfterBattleText:
+	text "How do you get"
+	line "out of here?"
+	done
+
+MtMoon1FSuperNerdBattleText:
+	text "What! Don't sneak"
+	line "up on me!"
+	done
+
+MtMoon1FSuperNerdEndBattleText:
+	text "My"
+	line "#MON won't do!"
+	prompt
+
+MtMoon1FSuperNerdAfterBattleText:
+	text "I have to find"
+	line "stronger #MON."
+	done
+
+MtMoon1FCooltrainerF2BattleText:
+	text "What? I'm waiting"
+	line "for my friends to"
+	cont "find me here."
+	done
+
+MtMoon1FCooltrainerF2EndBattleText:
+	text "I lost?"
+	prompt
+
+MtMoon1FCooltrainerF2AfterBattleText:
+	text "I heard there are"
+	line "some very rare"
+	cont "fossils here."
+	done
+
+MtMoon1FYoungster2BattleText:
+	text "Suspicious men"
+	line "are in the cave."
+	cont "What about you?"
+	done
+
+MtMoon1FYoungster2EndBattleText:
+	text "You"
+	line "got me!"
+	prompt
+
+MtMoon1FYoungster2AfterBattleText:
+	text "I saw them! I'm"
+	line "sure they're from"
+	cont "TEAM ROCKET!"
+	done
+
+MtMoon1FYoungster3BattleText:
+	text "Go through this"
+	line "cave to get to"
+	cont "CERULEAN CITY!"
+	done
+
+MtMoon1FYoungster3EndBattleText:
+	text "I"
+	line "lost."
+	prompt
+
+MtMoon1FYoungster3AfterBattleText:
+	text "ZUBAT is tough!"
+	line "But, it can be"
+	cont "useful if you"
+	cont "catch one."
+	done
+
+MtMoon1FBewareZubatSign:
+	text "Beware! ZUBAT is"
+	line "a blood sucker!"
+	done
 
 MtMoon1F_MapEvents:
 	db 0, 0 ; filler
--- maps/MtMoonB2F.asm
+++ maps/MtMoonB2F.asm
@@ -4,6 +4,137 @@ MtMoonB2F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	
+MtMoonJessieJamesText1:
+	text "Stop right there!"
+	done
+
+MtMoonJessieJamesText2:
+	text "That fossil is"
+	line "TEAM ROCKET's!"
+
+	para "Surrender now, or "
+	line "prepare to fight!"
+	done
+
+MtMoonJessieJamesText3:
+	text "A brat"
+	line "beat us?"
+	prompt
+
+MtMoonJessieJamesText4:
+	text "TEAM ROCKET, blast"
+	line "off at the speed"
+	cont "of light!@"
+	done
+
+MtMoonB2FDomeFossilYouWantText:
+	text "You want the"
+	line "DOME FOSSIL?"
+	done
+
+MtMoonB2FHelixFossilYouWantText:
+	text "You want the"
+	line "HELIX FOSSIL?"
+	done
+
+MtMoonB2FReceivedFossilText:
+	text "<PLAYER> got the"
+	line "@"
+	text_ram wcd6d
+	text "!@"
+	done
+
+MtMoonB2FYouHaveNoRoomText:
+	text "Look, you've got"
+	line "no room for this."
+	done
+
+MtMoonB2FSuperNerdTheyreBothMineText:
+	text "Hey, stop!"
+
+	para "I found these"
+	line "fossils! They're"
+	cont "both mine!"
+	done
+
+MtMoonB2FSuperNerdOkIllShareText:
+	text "OK!"
+	line "I'll share!"
+	prompt
+
+MtMoonB2fSuperNerdEachTakeOneText:
+	text "We'll each take"
+	line "one!"
+	cont "No being greedy!"
+	done
+
+MtMoonB2FSuperNerdTheresAPokemonLabText:
+	text "Far away, on"
+	line "CINNABAR ISLAND,"
+	cont "there's a #MON"
+	cont "LAB."
+
+	para "They do research"
+	line "on regenerating"
+	cont "fossils."
+	done
+
+MtMoonB2FSuperNerdThenThisIsMineText:
+	text "All right. Then"
+	line "this is mine!"
+	done
+
+MtMoonB2FRocket2BattleText:
+	text "We, TEAM ROCKET,"
+	line "are #MON"
+	cont "gangsters!"
+	done
+
+MtMoonB2FRocket2EndBattleText:
+	text "I blew"
+	line "it!"
+	prompt
+
+MtMoonB2FRocket2AfterBattleText:
+	text "Darn it all! My"
+	line "associates won't"
+	cont "stand for this!"
+	done
+
+MtMoonB2FRocket3BattleText:
+	text "We're pulling a"
+	line "big job here!"
+	cont "Get lost, kid!"
+	done
+
+MtMoonB2FRocket3EndBattleText:
+	text "So, you"
+	line "are good."
+	prompt
+
+MtMoonB2FRocket3AfterBattleText:
+	text "If you find a"
+	line "fossil, give it"
+	cont "to me and scram!"
+	done
+
+MtMoonB2FRocket4BattleText:
+	text "Little kids"
+	line "should leave"
+	cont "grown-ups alone!"
+	done
+
+MtMoonB2FRocket4EndBattleText:
+	text "I'm"
+	line "steamed!"
+	prompt
+
+MtMoonB2FRocket4AfterBattleText:
+	text "#MON lived"
+	line "here long before"
+	cont "people came."
+	done
 
 MtMoonB2F_MapEvents:
 	db 0, 0 ; filler
