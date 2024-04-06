f7d86543f213b307aad58b03ab18a188b35044ec
dreams897 <dreams897@proton.me>
2024-04-05 23:50:14 -0700

    Add missing SS Anne room warps

--- maps/SSAnneB1FRooms.asm
+++ maps/SSAnneB1FRooms.asm
@@ -9,12 +9,16 @@ SSAnneB1FRooms_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 22,  5, SS_ANNE_B1F, 3
-	warp_event 13,  5, SS_ANNE_B1F, 4
-	warp_event 12,  5, SS_ANNE_B1F, 4
-	warp_event  3,  5, SS_ANNE_B1F, 5
 	warp_event  2,  5, SS_ANNE_B1F, 5
+	warp_event  3,  5, SS_ANNE_B1F, 5
+	warp_event 12,  5, SS_ANNE_B1F, 4
+	warp_event 13,  5, SS_ANNE_B1F, 4
+	warp_event 22,  5, SS_ANNE_B1F, 3
 	warp_event 23,  5, SS_ANNE_B1F, 3
+	warp_event  2, 15, SS_ANNE_B1F, 2
+	warp_event  3, 15, SS_ANNE_B1F, 2
+	warp_event 12, 15, SS_ANNE_B1F, 1
+	warp_event 13, 15, SS_ANNE_B1F, 1
 
 	def_coord_events
 
