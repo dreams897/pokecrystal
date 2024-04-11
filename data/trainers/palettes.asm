8f20e2f6b13f9a782db8f53e1e46ffbedcdc0232
dreams897 <dreams897@proton.me>
2024-04-10 22:37:47 -0700

    Update Red's palettes

--- data/trainers/palettes.asm
+++ data/trainers/palettes.asm
@@ -6,8 +6,8 @@ TrainerPalettes:
 
 	table_width PAL_COLOR_SIZE * 2, TrainerPalettes
 
-PlayerPalette: ; Red uses the same colors as Cal
-INCBIN "gfx/trainers/cal.gbcpal", middle_colors
+PlayerPalette:
+INCBIN "gfx/trainers/red.gbcpal", middle_colors
 INCBIN "gfx/trainers/falkner.gbcpal", middle_colors
 INCBIN "gfx/trainers/whitney.gbcpal", middle_colors
 GreenPalette: ; Green/Leaf shares Bugsy's palette
