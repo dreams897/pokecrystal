b2427959a48d14acb4c27d8965cfff307e47ca7b
dreams897 <dreams897@proton.me>
2024-04-14 14:17:34 -0700

    Fix typo

--- data/battle_anims/framesets.asm
+++ data/battle_anims/framesets.asm
@@ -153,7 +153,7 @@ BattleAnimFrameData:
 	dw .Frameset_MindReader3         ; BATTLE_ANIM_FRAMESET_MIND_READER_3
 	dw .Frameset_MindReader4         ; BATTLE_ANIM_FRAMESET_MIND_READER_4
 	dw .Frameset_Safeguard           ; BATTLE_ANIM_FRAMESET_SAFEGUARD
-	dw .Frameset_MegapagerUnused     ; BATTLE_ANIM_FRAMESET_MEGAPAGER_UNUSED
+	dw .Frameset_MegapagerUnused     ; BATTLE_ANIM_FRAMESET_MEGAPHONE_UNUSED
 	dw .Frameset_ItemBag             ; BATTLE_ANIM_FRAMESET_ITEM_BAG
 	dw .Frameset_SpiderWeb           ; BATTLE_ANIM_FRAMESET_SPIDER_WEB
 	dw .Frameset_UnusedCake          ; BATTLE_ANIM_FRAMESET_UNUSED_CAKE
