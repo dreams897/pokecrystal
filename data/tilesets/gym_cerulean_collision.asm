f620b92891d802883c6c46ab45405497d533ccef
dreams897 <119394167+dreams897@users.noreply.github.com>
2024-05-01 23:03:35 -0700

    Cerulean Gym tileset improvements

--- data/tilesets/gym_cerulean_collision.asm
+++ data/tilesets/gym_cerulean_collision.asm
@@ -1,18 +1,28 @@
 	tilecoll WALL, WALL, WALL, WALL ; 00
-	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 01
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 02
+	tilecoll WALL, FLOOR, WALL, FLOOR ; 01
+	tilecoll WATER, WALL, WATER, WALL ; 02
 	tilecoll WALL, WALL, WALL, WALL ; 03
 	tilecoll FLOOR, FLOOR, WARP_CARPET_DOWN, WARP_CARPET_DOWN ; 04
-	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 05
-	tilecoll FLOOR, FLOOR, FLOOR, WALL ; 06
-	tilecoll FLOOR, FLOOR, WALL, FLOOR ; 07
-	tilecoll WALL, WALL, WALL, FLOOR ; 08
-	tilecoll WALL, WALL, FLOOR, WALL ; 09
-	tilecoll WALL, WALL, FLOOR, FLOOR ; 0a
-	tilecoll FLOOR, FLOOR, WALL, WALL ; 0b
-	tilecoll WALL, FLOOR, WALL, WALL ; 0c
-	tilecoll FLOOR, WALL, WALL, WALL ; 0d
-	tilecoll WALL, FLOOR, WALL, FLOOR ; 0e
-	tilecoll FLOOR, WALL, FLOOR, WALL ; 0f
-	tilecoll WALL, FLOOR, FLOOR, FLOOR ; 10
-	tilecoll FLOOR, WALL, FLOOR, FLOOR ; 11
+	tilecoll WATER, WATER, WATER, WATER ; 05
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 06
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 07
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 08
+	tilecoll FLOOR, WATER, FLOOR, WATER ; 09
+	tilecoll WALL, WALL, WATER, WATER ; 0a
+	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 0b
+	tilecoll FLOOR, WATER, FLOOR, WATER ; 0c
+	tilecoll WATER, FLOOR, WATER, FLOOR ; 0d
+	tilecoll FLOOR, FLOOR, WATER, WATER ; 0e
+	tilecoll FLOOR, WATER, FLOOR, WATER ; 0f
+	tilecoll WATER, WATER, WATER, WATER ; 10
+	tilecoll WATER, FLOOR, WATER, FLOOR ; 11
+	tilecoll WATER, WATER, FLOOR, FLOOR ; 12
+	tilecoll WATER, FLOOR, FLOOR, FLOOR ; 13
+	tilecoll WATER, WATER, FLOOR, FLOOR ; 14
+	tilecoll , , ,  ; 15
+	tilecoll , , ,  ; 16
+	tilecoll , , ,  ; 17
+	tilecoll , , ,  ; 18
+	tilecoll , , ,  ; 19
+	tilecoll , , ,  ; 1a
+	tilecoll , , ,  ; 1b
